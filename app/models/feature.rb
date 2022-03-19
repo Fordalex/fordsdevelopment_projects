36697cfdbbae5f4b2c6683039cf7c03346bb3e3d
Fordalex <alex96ford19@gmail.com>
2022-03-19 14:12:19 +0000

    add feature container and adds icons

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,3 +1,8 @@
 class Feature < ApplicationRecord
   belongs_to :project
+  has_many :technology_groups, as: :technology_groupable, dependent: :destroy
+
+  def technologies
+    technology_groups.map { |tg| tg.technology }
+  end
 end
