963affe876938ad125428190be56f9d03d5af40e
Fordalex <alex96ford19@gmail.com>
2022-03-20 13:39:43 +0000

    adds more fields for project form and adds new path

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -3,6 +3,21 @@ module Admin
     before_action :set_profile, only: %i[destroy edit update].freeze
     layout "admin"
 
+    def new
+      @project = Project.new
+    end
+
+    def create
+      @project = Project.new(profile_params)
+      if @project.save!
+        flash[:success] = "Project was successfully created."
+      else
+        flash[:warning] = "An error occurred, please try again."
+      end
+      create_technology_groups
+      redirect_to admin_projects_path(@project)
+    end
+
     def destroy
       @project.destroy
       redirect_to admin_projects_path
@@ -12,6 +27,7 @@ module Admin
     end
 
     def update
+      delete_technology_groups
       create_technology_groups
       if @project.update(profile_params)
         flash[:success] = "Project was successfully updated."
@@ -27,8 +43,11 @@ module Admin
 
     private
 
-    def create_technology_groups
+    def delete_technology_groups
       @project.technology_groups.destroy_all
+    end
+
+    def create_technology_groups
       params[:technologies].each do |key, value|
         technology = Technology.find_by(name: key)
         TechnologyGroup.create(
@@ -43,7 +62,7 @@ module Admin
     end
 
     def profile_params
-      params.require(:project).permit(:name)
+      params.require(:project).permit(:name, :order, :link, :image, :description, :ux)
     end
   end
 end
