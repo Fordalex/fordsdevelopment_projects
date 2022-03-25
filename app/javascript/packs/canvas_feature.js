eec2c1bbae28219bcaef9fb860cf7bb5e12b6137
Fordalex <alex96ford19@gmail.com>
2022-03-25 21:26:28 +0000

    updates the firework feature

--- app/javascript/packs/canvas_feature.js
+++ app/javascript/packs/canvas_feature.js
@@ -1,38 +1,49 @@
 class Partical {
   constructor(posX, posY) {
     this.explosionPower = 1;
-      this.diameter = 1;
-      this.position = {
-          x: posX,
-          y: posY,
-      }
-      this.velocity = {
-          x: -6 + Math.floor(Math.random() * 12),
-          y: -6 + Math.floor(Math.random() * 12),
-      }
+    this.diameter = Math.random() * 2;
+    this.alpha = Math.random() * 6;
+    this.position = {
+        x: posX,
+        y: posY,
+    }
+    this.velocity = {
+        x: Math.random() < 0.5 ? -Math.abs(Math.random()) * 3 : Math.random() * 3,
+        y: Math.random() < 0.5 ? -Math.abs(Math.random()) * 3 : Math.random() * 3
+    }
+    this.green = Math.floor(Math.random() * 210)
+    this.blue = Math.floor(Math.random() * 210)
   }
   draw() {
     ctx.moveTo(this.position.x, this.position.y)
+    ctx.beginPath();
+    ctx.fillStyle = `rgba(255, ${this.green}, ${this.blue}, ${this.alpha})`;
     ctx.arc(this.position.x, this.position.y, this.diameter, 0, 2 * Math.PI);
-    ctx.strokeStyle = '#ff0000';
-    ctx.stroke();
+    ctx.fill();
   }
   update() {
+    this.alpha -= this.diameter / 50;
     this.position.y += this.velocity.y;
     this.position.x += this.velocity.x;
-    this.velocity.y += 0.1;
+    this.velocity.y += this.diameter / 100;
+    if (this.velocity.x > 0.5) {
+      this.velocity.x -= this.diameter / 120;
+    }
+    if (this.velocity.x < -0.5) {
+      this.velocity.x += this.diameter / 120;
+    }
   }
 }
 class Firework {
   constructor() {
-    this.diameter = 1;
+    this.diameter = 2;
     this.position = {
-      x: 200 + (Math.floor(Math.random() * 300)),
+      x: 750 + (Math.floor(Math.random() * 100)),
       y: 500,
     }
     this.velocity = {
       x: 0,
-      y: Math.floor(Math.random() * 5) + -10,
+      y: Math.floor(Math.random() * 2) + -12,
     }
     this.reduce = 0;
     this.exploded = false;
@@ -41,25 +52,30 @@ class Firework {
   draw() {
     if (!this.exploded) {
       ctx.moveTo(this.position.x, this.position.y)
+      ctx.beginPath();
       ctx.arc(this.position.x, this.position.y, this.diameter, 0, 2 * Math.PI);
-      ctx.strokeStyle = '#ff0000';
-      ctx.stroke();
+      ctx.fillStyle = `rgba(255, 100, 20, ${this.alpha})`;
+      ctx.fill();
     }
   }
   explode() {
-    reset();
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 200; i++) {
         particals.push(new Partical(this.position.x, this.position.y));
     }
+    ctx.moveTo(this.position.x, this.position.y)
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, 10, 0, 2 * Math.PI);
+    ctx.fillStyle = "#fff";
+    ctx.fill();
   }
   update() {
     if (!this.exploded) {
-      this.position.y += this.velocity.y;
+      this.position.y += (this.velocity.y / 5);
       if (this.reduce == 8) {
         this.velocity.y++;
         this.reduce = 0;
       }
-      this.reduce++;
+      this.reduce += 0.5;
       if (this.velocity.y > 0) {
         this.exploded = true;
         this.alpha = 0;
@@ -74,8 +90,10 @@ var ctx = c.getContext("2d");
 var firework = new Firework;
 
 function draw() {
-  ctx.clearRect(0, 0, 500, 500);
   ctx.beginPath();
+  ctx.rect(0, 0, 1250, 530);
+  ctx.fillStyle = "rgba(0,0,0,0.1)"
+  ctx.fill();
 
   firework.update();
   firework.draw();
@@ -87,7 +105,7 @@ function draw() {
 
   setTimeout(() => {
     draw();
-  }, 30)
+  }, 10)
 }
 
 function reset() {
@@ -100,3 +118,13 @@ function reset() {
 var particals = [];
 
 draw();
+
+function shootFirework() {
+  firework = new Firework;
+  particals = [];
+}
+
+
+// Add event to firework button
+var fireworkButton = document.getElementById('fireworkButton');
+fireworkButton.addEventListener('click', shootFirework)
