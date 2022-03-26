2ff78e90f91e8934051935ced03d89862c99428c
Fordalex <alex96ford19@gmail.com>
2022-03-26 09:49:44 +0000

    updates the firework feature

--- app/javascript/packs/canvas_feature.js
+++ app/javascript/packs/canvas_feature.js
@@ -1,8 +1,49 @@
+class Spark {
+  constructor(posX, posY, alpha, velocityX, velocityY) {
+    this.diameter = 1;
+    this.alpha = alpha;
+    this.position = {
+        x: posX,
+        y: posY,
+    }
+    this.velocity = {
+        x: velocityX,
+        y: velocityY
+    }
+    this.green = Math.floor(Math.random() * 210) + 150
+    this.blue = Math.floor(Math.random() * 210) + 150
+  }
+  draw() {
+    ctx.moveTo(this.position.x, this.position.y)
+    ctx.beginPath();
+    ctx.fillStyle = `rgba(255, ${this.green}, ${this.blue}, ${this.alpha})`;
+    ctx.arc(this.position.x, this.position.y, this.diameter, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  update() {
+    this.alpha -= this.diameter / 4;
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+    this.velocity.y += this.diameter / 100;
+    if (this.velocity.x > 0.5) {
+      this.velocity.x -= this.diameter / 120;
+    }
+    if (this.velocity.x < -0.5) {
+      this.velocity.x += this.diameter / 120;
+    }
+    if (this.alpha < 0 || this.position.y > 600) {
+      this.destroy();
+    }
+  }
+  destroy() {
+    index = sparks.indexOf(this)
+    sparks.splice(index, 1)
+  }
+}
 class Partical {
   constructor(posX, posY) {
-    this.explosionPower = 1;
     this.diameter = Math.random() * 2;
-    this.alpha = Math.random() * 6;
+    this.alpha = Math.random() * 3 + 1;
     this.position = {
         x: posX,
         y: posY,
@@ -20,18 +61,34 @@ class Partical {
     ctx.fillStyle = `rgba(255, ${this.green}, ${this.blue}, ${this.alpha})`;
     ctx.arc(this.position.x, this.position.y, this.diameter, 0, 2 * Math.PI);
     ctx.fill();
+    this.spark();
+  }
+  spark() {
+    velocityX = (Math.random() < 0.5 ? -Math.random() : Math.random()) * this.velocity.x;
+    velocityY = (Math.random() < 0.5 ? -Math.random() : Math.random()) * this.velocity.y;
+    alpha = this.alpha / 2;
+    sparks.push(new Spark(this.position.x, this.position.y, alpha, velocityX, velocityY));
   }
   update() {
     this.alpha -= this.diameter / 50;
     this.position.y += this.velocity.y;
     this.position.x += this.velocity.x;
     this.velocity.y += this.diameter / 100;
+    this.velocity.y += 0.01;
+
     if (this.velocity.x > 0.5) {
       this.velocity.x -= this.diameter / 120;
     }
     if (this.velocity.x < -0.5) {
       this.velocity.x += this.diameter / 120;
     }
+    if (this.alpha < 0 || this.position.y > 600) {
+      this.destroy();
+    }
+  }
+  destroy() {
+    index = particals.indexOf(this)
+    particals.splice(index, 1)
   }
 }
 class Firework {
@@ -43,7 +100,7 @@ class Firework {
     }
     this.velocity = {
       x: 0,
-      y: Math.floor(Math.random() * 2) + -12,
+      y: -12.7
     }
     this.reduce = 0;
     this.exploded = false;
@@ -56,11 +113,18 @@ class Firework {
       ctx.arc(this.position.x, this.position.y, this.diameter, 0, 2 * Math.PI);
       ctx.fillStyle = `rgba(255, 100, 20, ${this.alpha})`;
       ctx.fill();
+      this.spark()
     }
+  }
+  spark() {
+    velocityY = -this.velocity.y / 50;
+    velocityX = Math.random() < 0.5 ? velocityY : -velocityY;
+    alpha = Math.random() * 4;
+    sparks.push(new Spark(this.position.x, this.position.y, alpha, velocityX, velocityY));
   }
   explode() {
     for (let i = 0; i < 200; i++) {
-        particals.push(new Partical(this.position.x, this.position.y));
+      particals.push(new Partical(this.position.x, this.position.y));
     }
     ctx.moveTo(this.position.x, this.position.y)
     ctx.beginPath();
@@ -103,19 +167,22 @@ function draw() {
     p.update();
   }
 
+  for (s of sparks) {
+    s.draw();
+    s.update();
+  }
+
   setTimeout(() => {
     draw();
   }, 10)
-}
 
-function reset() {
-  setTimeout(() => {
-    firework = new Firework;
-    particals = []
-  }, 7000)
+  console.log("Spark", sparks.length)
+  console.log("Particals", particals.length)
+  console.log("Fireworks", firework)
 }
 
 var particals = [];
+var sparks = [];
 
 draw();
 
