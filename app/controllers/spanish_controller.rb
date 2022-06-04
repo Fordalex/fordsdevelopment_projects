1a926b05e243fb7e2050c62c669634bdb3978012
Fordalex <alex96ford19@gmail.com>
2022-06-04 13:58:04 +0100

    allow any site to make a request to this route.

--- app/controllers/spanish_controller.rb
+++ app/controllers/spanish_controller.rb
@@ -1,4 +1,12 @@
 class SpanishController < ApplicationController
+  before_action :set_headers
+
   def feed
+  end
+
+  protected
+
+  def set_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
   end
 end
