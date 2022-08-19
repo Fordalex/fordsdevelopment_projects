6e26ada6861ea1ddfb048a80ed58bb9fc7af5755
Fordalex <alex96ford19@gmail.com>
2022-08-19 23:44:33 +0100

    order flash cards

--- app/controllers/quiz/flash_card_categories_controller.rb
+++ app/controllers/quiz/flash_card_categories_controller.rb
@@ -7,7 +7,7 @@ module Quiz
     def play
       category_ids = []
       params[:categories].each { |k,v| v === "1" ? category_ids << k : next }
-      @flash_cards = FlashCard.where(flash_card_category_id: category_ids)
+      @flash_cards = FlashCard.where(flash_card_category_id: category_ids).order(correct: :asc, incorrect: :desc)
       # TODO don't render from a post request
       render 'play'
     end
